4b278a5c0a2bd5875b4588d283efbaea3ec36f7d
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-06-09 19:30:50 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -1,59 +1,32 @@
 import streamlit as st
-from yfinance import Ticker 
-import warnings
-from datetime import datetime
-from datetime import date
+import mysql.connector
+import pandas as pd
+import datetime
 import plotly.graph_objects as go  
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
 
-def fetch_aapl_data(start_date, end_date):
-  aapl = Ticker("AAPL")  # Create a Ticker object for AAPL
-  # Download historical data (adjust period as needed)
-  data = aapl.history(start=start_date, end=end_date)
-  return data
+#Coneccion y Fuente de Datos
+hostname = "db-vtiger-tableau-ar.ca4cxchfpbn1.sa-east-1.rds.amazonaws.com" # vtiger prod(lectura)
+username = "agdb"
+password = "AGvtigerXLS"
+database = "agrired-vtiger"
+connection = mysql.connector.connect(host=hostname,user=username,password=password,database=database)
+cursor = connection.cursor()
 
-# Initial date range (you can customize these)
-today = date.today().strftime("%Y-%m-%d")
+query = "SELECT * FROM `agrired-vtiger`.`vtiger_account`;"
+cursor.execute(query)
+rows = cursor.fetchall()
+vtiger = pd.DataFrame(rows, columns=[i[0] for i in cursor.description]) 
 
-# Initial date range (you can customize these)
-start_date = "2024-01-01"
-end_date = today
-
-# Create a sidebar for user input
-st.sidebar.title("Date Filter")
-date_format = "%Y-%m-%d"  # Adjust format if needed
-start_date_obj = datetime.strptime(start_date, date_format)
-end_date_obj = datetime.strptime(end_date, date_format)
-new_start_date = st.sidebar.date_input("Start Date", value=start_date_obj)
-new_end_date = st.sidebar.date_input("End Date", value=end_date_obj)
+#Procesamiento de Datos
 
 
-data = None  # Initialize data as None
-# Button to apply changes and refetch data
-if st.sidebar.button("Apply deherChanges"):
-  # Update data based on user input
-  start_date = new_start_date
-  end_date = new_end_date
-  data = fetch_aapl_data(start_date, end_date)  # Refetch data
 
-# Display the title and descriptive text
-st.title("My Online Dashboard")
-st.write("This dashboard displays information about Apple (AAPL) stock.")
+#GUI
+st.title("Tablero Empresas")
+st.dataframe(vtiger)
 
-
-# Display the fetched data (assuming data is a pandas DataFrame)
-if data is not None:
-    # Create a Plotly figure
-    fig = go.Figure(data=[go.Scatter(x=data.index, y=data['Close'])])
-    fig.update_layout(title='AAPL Stock Price', xaxis_title='Date', yaxis_title='Price - chotocorta')
-    st.plotly_chart(fig)  # Display the chart
-else:
-    st.write("No data available for the selected date range.")
-
-# Button to refresh data (optional)
-if st.button("Refresh Data"):
-    data = fetch_aapl_data(start_date, end_date)  # Refetch data
+cursor.close()
+connection.close()
 
 
 
