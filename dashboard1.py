206f8931487373776858b4624b8d88676dcf429c
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-06-09 16:41:07 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -1,13 +1,6 @@
-'''
-streamlit run C:/Users/Usuario/Desktop/dashboard1.py
-'''
 
 import streamlit as st
 from yfinance import Ticker 
-import warnings
-
-from datetime import datetime
-from datetime import date
 
 
 
