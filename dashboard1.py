fd2af1015c872957cad67ef887bd543b02b031a7
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-06-10 00:15:17 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -60,7 +60,7 @@ start_date0 = pd.to_datetime("2011-01-03").date()
 end_date=date.today()
 start_date1 = pd.to_datetime("2024-01-01").date()
 
-new_start_date, new_end_date = st.slider("Fecha", start_date1, end_date, (start_date0, end_date))
+new_start_date, new_end_date = st.slider("Fecha", start_date0, end_date, (start_date1, end_date))
 chosen = st.radio("Seleccionar Dolar", ("blue", "mayorista", "oficial", "contadoconliqui", "bolsa", "tarjeta", "cripto"), horizontal=True, key="sorting_hat_radio")
 
 with st.spinner("Fetching data..."):  # Display spinner while fetching data
