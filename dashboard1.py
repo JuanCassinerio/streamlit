6061189fb38906a9f4f545349394a90c55cbb8f4
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-06-10 00:33:46 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -54,7 +54,7 @@ def dolar(start_date_date, end_date):
 
 #GUI
 
-st.title("Cotizacion de los diferentes dolares a tiempo real")
+st.markdown("Cotizacion de los diferentes dolares a tiempo real",color="blue")
 st.markdown("<p style='text-align: center;'>Para mas informacion ir a <a href='https://juancassinerio.wixsite.com/finance'>https://juancassinerio.wixsite.com/finance/</a>. Desarrollo de Algoritmos financieros. Procesamiento y Desarrollo de modelos econometricos</p>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Fuentes: <a href='https://dolarapi.com'>https://dolarapi.com/</a> y <a href='https://argentinadatos.com/'>https://argentinadatos.com/</a></p>", unsafe_allow_html=True)
 
