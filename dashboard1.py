69b3d6b35d78f85e5bf4dc5517a139c7deddcfed
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-06-10 00:55:03 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -68,6 +68,7 @@ chosen = st.radio("Seleccionar Dolar", ("blue", "mayorista", "oficial", "contado
 
 with st.spinner("Fetching data..."):  # Display spinner while fetching data
     dolar=dolar(new_start_date, new_end_date)
+    dolar = dolar.drop_duplicates(subset='fecha', keep='first')
 
 fig = px.line(dolar, x='fecha', y=chosen)
 fig.update_layout(title=dict(text=f'Dolar {chosen}',x=0.5,xanchor='center',font=dict(color="black", size=14)))
