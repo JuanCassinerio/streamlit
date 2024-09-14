658ea1eea4c48109586888734e8376a7b590efd6
Juan Cassinerio <113950415+JuanCassinerio@users.noreply.github.com>
2024-09-14 12:23:29 -0300

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -60,7 +60,7 @@ st.markdown("Para mas informacion ir a <a href='https://juancassinerio.wixsite.c
 chosen ='contadoconliqui'
 start_date0 = pd.to_datetime("2024-01-03").date()
 end_date=date.today()
-start_date1 = end_date - timedelta(days=30 * 4)  #4 months
+start_date1 = end_date - timedelta(days=30 * 6)  #4 months
 
 
 new_start_date, new_end_date = st.slider("Fecha",start_date0, end_date,  (start_date1, end_date))
