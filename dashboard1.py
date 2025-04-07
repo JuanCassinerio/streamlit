a0f50d9f9acb857ecfd400af1bf06a1f47d24c1e
x1juacas <jcassinerio@fyo.com>
2025-04-07 15:55:45 -0300

    cambio

--- dashboard1.py
+++ dashboard1.py
@@ -14,27 +14,25 @@ st.title("Tablero Empreafdefewfsas🎈")
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
 
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
+st.text_input("Your name", key="name")
+st.session_state.name
 
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
 
-st.code(f"""
-import streamlit as st
 
-st.markdown('''''')
-""")
+
+add_slider = st.sidebar.slider('Select a range of values',0.0, 100.0, (25.0, 75.0)) #filtro barra
+
+
+chosen = st.radio('Sorting hat',("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin")) #visible filtro selecion individual
+
+
+
+
 
 st.markdown("""
     <style>
