2e8241c14f6e7f7370969cb8588b8f305dd28149
x1juacas <jcassinerio@fyo.com>
2025-04-07 09:57:55 -0300

    cambio

--- dashboard1.py
+++ dashboard1.py
@@ -53,7 +53,7 @@ def dolar(start_date_date, end_date):
   return filtered_df
 
 #GUI
-st.markdown('''<h1 style="color: grey; font-size: 50px; font-weight: bold;">Dolar Ya</h1>''',unsafe_allow_html=True)
+st.markdown('''<h1 style="color: grey; font-size: 50px; font-weight: bold;">Dolar Yaaaaa</h1>''',unsafe_allow_html=True)
 st.markdown("Cotizacion en vivo e historica del dolar. Fuentes: dolarapi.com / argentinadatos.com", unsafe_allow_html=True)
 #st.markdown("Para mas informacion ir a <a href='https://juancassinerio.wixsite.com/finance'>https://juancassinerio.wixsite.com/finance/</a>", unsafe_allow_html=True)
 
