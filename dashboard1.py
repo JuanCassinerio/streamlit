9a7142c48809d9cc0567abebf8665f95d28fd4b3
x1juacas <jcassinerio@fyo.com>
2025-04-07 10:31:28 -0300

    cambio

--- dashboard1.py
+++ dashboard1.py
@@ -5,59 +5,36 @@ from datetime import date, timedelta
 
 # streamlit run "C:/Users/Usuario/Desktop/Scripts/Nueva carpeta/dark box/st.py" ""to deal with folders with spaces
 
-
 from functions import dolar
+
 #GUI
 st.markdown('''<h1 style="color: grey; font-size: 50px; font-weight: bold;">Dolar Ya</h1>''',unsafe_allow_html=True)
-st.markdown('''<h3 style="color: black; font-size: 15px; font-weight: bold;">Hecho ppor Juan Cassinerio</h3>''', unsafe_allow_html=True)
-
 st.markdown("Cotizacion en Hivo e Historica (Fuentes: dolarapi.com / argentinadatos.com)", unsafe_allow_html=True)
-#st.markdown("Para mas informacion ir a <a href='https://juancassinerio.wixsite.com/finance'>https://juancassinerio.wixsite.com/finance/</a>", unsafe_allow_html=True)
+st.markdown('''<h3 style="color: black; font-size: 15px; font-weight: bold;">Hecho por Juan Cassinerio</h3>''', unsafe_allow_html=True)
+
+
+st.markdown("Para mas informacion ir a <a href='https://juancassinerio.wixsite.com/finance'>https://juancassinerio.wixsite.com/finance/</a>", unsafe_allow_html=True)
+
+
+
+opciones_dolar = {"blue": "Blue","mayorista": "Mayorista","oficial": "Oficial","contadoconliqui": "CCL","bolsa": "Bolsa","tarjeta": "Tarjeta","cripto": "Cripto"}
+chosen = st.radio("Seleccionar Dolar", tuple(opciones_dolar.values()), horizontal=True, key="sorting_hat_radio") #dolar type button
+chosen_key = [clave for clave, valor in opciones_dolar.items() if valor == chosen][0]
 
 
 start_date0 = pd.to_datetime("2024-01-03").date()
 end_date=date.today()
 start_date1 = end_date - timedelta(days=30 * 3)  #4 months
-
-
-new_start_date, new_end_date = st.slider("Fecha",start_date0, end_date,  (start_date1, end_date))
-
-opciones_dolar = {
-    "blue": "Blue",
-    "mayorista": "Mayorista",
-    "oficial": "Oficial",
-    "contadoconliqui": "CCL",
-    "bolsa": "Bolsa",
-    "tarjeta": "Tarjeta",
-    "cripto": "Cripto"
-}
-
-# Crear el radio button utilizando los nuevos nombres
-chosen = st.radio("Seleccionar Dolar", tuple(opciones_dolar.values()), horizontal=True, key="sorting_hat_radio")
-
-# Obtener el valor original (clave) a partir del valor seleccionado (valor)
-chosen_key = [clave for clave, valor in opciones_dolar.items() if valor == chosen][0]
+new_start_date, new_end_date = st.slider("Fecha",start_date0, end_date,  (start_date1, end_date)) #time slider
 
 with st.spinner("Fetching data..."):
-    dolar = dolar(new_start_date, new_end_date)
+    dolar = dolar(new_start_date, new_end_date) #api data query
   
 fig = px.line(dolar, x='fecha', y=chosen_key)
-fig.update_layout(
-    title=dict(
-        text=f'Dolar {chosen} - {new_start_date}/{new_end_date}', 
-        x=0.5, 
-        xanchor='center', 
-        font=dict(color="black", size=14)
-    ),
-    yaxis_title=None,  # Setting the y-axis title as [$]
-    xaxis_title=None,  # Removing x-axis title
-)
-
-# Rotating the y-axis title horizontally (by setting angle to 0)
-fig.update_yaxes(
-    tickformat="$",  # Add the dollar symbol to labels
-      # Alternatively, add the symbol as a suffix
-)
+fig.update_layout(title=dict(
+        text=f'Dolar {chosen} - {new_start_date}/{new_end_date}', x=0.5, xanchor='center',
+        font=dict(color="black", size=14)),yaxis_title=None,xaxis_title=None,)
+fig.update_yaxes(tickformat="$")
 
 st.plotly_chart(fig)
 
