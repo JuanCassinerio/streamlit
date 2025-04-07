1778768b66aab8ec58a77eee3babb97c901b2035
x1juacas <jcassinerio@fyo.com>
2025-04-07 16:27:58 -0300

    cambio

--- dashboard1.py
+++ dashboard1.py
@@ -13,8 +13,8 @@ import streamlit as st
 st.set_page_config(page_title="Dolar Ya", page_icon="💸", layout="centered")
 
 st.sidebar.markdown("# Navegación")
-st.sidebar.page_link("st.py", label="🏠 Inicio")
-st.sidebar.page_link("pages/dashbpard.py", label="📈 Histórico")
+st.sidebar.page_link("dashboard1.py", label="🏠 Inicio")
+
 st.sidebar.page_link("pages/data.py", label="🧪 Otro análisis")
 
 st.markdown('''<h1 style="color: green; font-size: 50px; font-weight: bold;">Dolar Ya</h1>''',unsafe_allow_html=True)
