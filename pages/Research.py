60071f868da80d221c269815d9db7f23494ee7d5
x1juacas <jcassinerio@fyo.com>
2025-04-08 11:01:25 -0300

    cambio

--- pages/Research.py
+++ pages/Research.py
@@ -1,35 +1,7 @@
 import streamlit as st
 #Navegacion
-st.markdown("""
-    <style>
-        /* Hide default page navigation */
-        [data-testid="stSidebarNavItems"] {
-            display: none !important;
-        }
-
-        /* Optional: reduce padding where the nav was */
-        [data-testid="stSidebarNav"] > div:nth-child(1) {
-            padding-top: 0rem;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-st.sidebar.markdown("# Navegación")
-st.sidebar.page_link("dashboard1.py", label="🏠 Inicio")
-st.sidebar.page_link("pages/portfolio.py", label="💰 Mi Portfolio")
-st.sidebar.page_link("pages/dolar.py", label="💲 Dolar Hoy")
-st.sidebar.markdown(" ")
-st.sidebar.page_link("pages/data.py", label="💸 Acciones y Cedears")
-st.sidebar.page_link("pages/data.py", label="🧾 Bonos")
-st.sidebar.page_link("pages/data.py", label="🪙 Crypto")
-
-
-
-st.sidebar.markdown("---")  # Optional horizontal line separator
-
-st.sidebar.markdown("## Más")
-st.sidebar.page_link("pages/contact.py", label="📞 Contacto")
-st.sidebar.page_link("pages/Research.py", label="🧠 Research")
+from sidebar import render_sidebar
+render_sidebar()
 
 
 
---
+++ pages/sidebar.py
@@ -0,0 +1,29 @@
+# sidebar.py
+import streamlit as st
+
+def render_sidebar():
+    st.markdown("""
+        <style>
+            [data-testid="stSidebarNavItems"] {
+                display: none !important;
+            }
+            [data-testid="stSidebarNav"] > div:nth-child(1) {
+                padding-top: 0rem;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.sidebar.markdown("# Navegación")
+    st.sidebar.page_link("dashboard1.py", label="🏠 Inicio")
+    st.sidebar.page_link("pages/portfolio.py", label="💰 Mi Portfolio")
+    st.sidebar.page_link("pages/dolar.py", label="💲 Dolar Hoy")
+
+    st.sidebar.markdown(" ")
+    st.sidebar.page_link("pages/data.py", label="💸 Acciones y Cedears")
+    st.sidebar.page_link("pages/data.py", label="🧾 Bonos")
+    st.sidebar.page_link("pages/data.py", label="🪙 Crypto")
+
+    st.sidebar.markdown("---")
+    st.sidebar.markdown("## Más")
+    st.sidebar.page_link("pages/contact.py", label="📞 Contacto")
+    st.sidebar.page_link("pages/Research.py", label="🧠 Research")
